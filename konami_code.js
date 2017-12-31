492c8d5330be5f39a0469b502412bfd39c1022bb
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-31 07:01:20 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -7,18 +7,17 @@ function init() {
   // Write your JavaScript code inside the init() function
         function onKeyDownHandler(e) {
           const key = parseInt(e.detail); // || e.which)
-          alert(key)
-          // if (key === alphabet[index]) {
-          //   index++;
-          //
-          //   if (index === alphabet.length) {
-          //     alert("Hurray!");
-          //
-          //     index = 0;
-          //   }
-          // } else {
-          //   index = 0;
-          // }
+          if (key === alphabet[index]) {
+            index++;
+          
+            if (index === alphabet.length) {
+              alert("Hurray!");
+          
+              index = 0;
+            }
+          } else {
+            index = 0;
+          }
         }
 }
 
@@ -34,4 +33,3 @@ input.addEventListener('keydown', function(e) {
 let index = 0;
 
 // This is the function that would be invoked by the event listener.
-
