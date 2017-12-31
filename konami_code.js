7da46a76f304c2861dc87bc025b4f7b7bdecc66c
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-31 07:02:33 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -7,18 +7,19 @@ let index = 0;
 function init() {
   // Write your JavaScript code inside the init() function
         function onKeyDownHandler(e) {
-          const key = parseInt(e.detail); // || e.which)
-          if (key === alphabet[index]) {
-            index++;
-
-            if (index === alphabet.length) {
-              alert("Hurray!");
-
-              index = 0;
-            }
-          } else {
-            index = 0;
-          }
+          alert('Hello World')
+          // const key = parseInt(e.detail); // || e.which)
+          // if (key === alphabet[index]) {
+          //   index++;
+          // 
+          //   if (index === alphabet.length) {
+          //     alert("Hurray!");
+          // 
+          //     index = 0;
+          //   }
+          // } else {
+          //   index = 0;
+          // }
         }
 }
 
