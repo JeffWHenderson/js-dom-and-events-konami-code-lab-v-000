0369a2eb2c5c0267bceaa7872c90b849e6e87bfa
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-31 07:22:43 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 // up, up, down, down, left, right, left, right, B, A
 let index = 0;
-function init() {
+
         document.querySelector('body').addEventListener('keydown', function(e) {
           const key = parseInt(e.which)
 
@@ -18,4 +18,4 @@ function init() {
           }
 
         })
-}
+
