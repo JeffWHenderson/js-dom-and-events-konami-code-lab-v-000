6c71e7d333e5809ed19aed3ba95ca6b3ee2461db
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-31 07:19:23 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,11 +1,11 @@
-const code = [38]//, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 // up, up, down, down, left, right, left, right, B, A
 let index = 0;
 //let key = ""
 
 document.querySelector('body').addEventListener('keydown', function(e) {
   const key = parseInt(e.which)
-  
+
    if (key === code[index]) {
      alert("level 1")
      index++;
