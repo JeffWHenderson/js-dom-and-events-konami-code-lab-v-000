7cb3f40797360ebc88d828bc6aceddd80ee26258
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-31 07:21:09 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -20,31 +20,3 @@ function init() {
         })
 }
 
-
-
-  // Write your JavaScript code inside the init() function
-        // function onKeyDownHandler(e) {
-        //   alert('Hello World')
-          // const key = parseInt(e.detail); // || e.which)
-          // if (key === code[index]) {
-          //   index++;
-          //
-          //   if (index === code.length) {
-          //     alert("Hurray!");
-          //
-          //     index = 0;
-          //   }
-          // } else {
-          //   index = 0;
-          // }
-        // }
-
-
-
-
-// Keep track of index outside of the event handler.
-
-
-// Keep track of index outside of the event handler.
-
-// This is the function that would be invoked by the event listener.
