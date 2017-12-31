56c9e4eeda637c9e3c5a691124dac3efcc54160f
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-31 07:25:18 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+// up, up, down, down, left, right, left, right, B, A
 
 function init() {
-  // Write your JavaScript code inside the init() function
+let index = 0;
 
+        document.querySelector('body').addEventListener('keydown', function(e) {
+          const key = parseInt(e.which || e.detail)
+
+           if (key === code[index]) {
+             index++;
+
+             if (index === code.length) {
+              alert("Flawless Victory!");
+
+              index = 0;
+            }
+          } else {
+            index = 0;
+          }
+
+        })
 }
